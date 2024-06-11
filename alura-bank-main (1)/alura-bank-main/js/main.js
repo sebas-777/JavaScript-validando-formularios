3bbe86d8faf6555350b22a5188cdc85fc83a2868
Sebastian Castro <scastro@qvision.com.co>
2024-06-11 18:02:16 -0500

    [sc] second commit

--- alura-bank-main (1)/alura-bank-main/js/main.js
+++ alura-bank-main (1)/alura-bank-main/js/main.js
@@ -1,9 +1,43 @@
+import esUnCuil from "./validar-cuil.js";
+import esMayorDeEdad from "./validar-edad.js";
+import {tiposError,mensajes} from "./customErrors.js"
+
 const campoDeFormulario = document.querySelectorAll('[required]');
 
 campoDeFormulario.forEach((campo)=>{
-    campo.addEventListener('blur',()=> verificarCampo(campo))
+    campo.addEventListener('blur',()=> verificarCampo(campo));
+    campo.addEventListener('invalid',evento => evento.preventDefault());
 }); 
 
-function verificarCampo(campo) {
-    console.log('Cambio',campo.name);
-}
+function verificarCampo(campo) { 
+    let mensaje=""
+    if (campo.name =="cuil" && campo.value.length >= 11) {
+        esUnCuil(campo);
+    }
+
+    if(campo.name == 'fecha_nacimiento' && campo.value != ''){
+       esMayorDeEdad(campo);
+    }
+    // console.log(campo.validity);
+
+    tiposError.forEach(error=>{
+        if(campo.validity[error]){
+            mensaje =mensajes[campo.name][error]
+            console.log(mensaje)
+        }
+    })
+
+    const mensajeError = campo.parentNode.querySelector('.mensaje-error');
+
+    const validarInputCheck = campo.checkValidaty();
+
+    if(!validarInputCheck){
+        mensajeError.textContent = mensaje;
+    } else{
+        mensajeError.textContent = '';
+    }
+} 
+
+
+  
+
---
+++ alura-bank-main (1)/alura-bank-main/js/validar-edad.js
@@ -0,0 +1,15 @@
+export default function esMayorDeEdad(campo){
+    const fechaNacimiento = new Date(campo.value);
+    validarEdad(fechaNacimiento);
+    console.log(validarEdad(fechaNacimiento));
+} 
+
+function validarEdad(fecha) {
+    const fechaActual = new Date();
+    const fechaMas18 = new Date(
+        fecha.getUTCFullYear() + 18,
+        fecha.getUTCMonth(),
+        fecha.getUTCDate()
+    );
+    return fechaActual >= fechaMas18;    
+}
